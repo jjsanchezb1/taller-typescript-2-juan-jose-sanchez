02c35829b574315410475c4baf78eca518ec3122
jjsanchezb1 <jj.sanchezb1@uniandes.edu.co>
2026-04-13 11:01:50 -0500

    cambio y reescritura de funcion nueva de main + html

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ import { series } from "./dataSeries.js";
 
 const seriesTable: HTMLElement = document.getElementById("series-body")!;
 const promedioTemporadas: HTMLElement = document.getElementById("promedio-temporadas")!;
+const detallesSerie: HTMLElement = document.getElementById("detalles-serie")!;
 
 mostrarSeries(series);
 promedioTemporadas.innerHTML = `Promedio Temporadas: ${getPromedioTemporadas(series)}`;
@@ -9,13 +10,19 @@ promedioTemporadas.innerHTML = `Promedio Temporadas: ${getPromedioTemporadas(ser
 function mostrarSeries(seriesList: any[]): void {
     seriesList.forEach(serie => {
         let trElement = document.createElement("tr");
+        trElement.style.cursor = "pointer";
         trElement.innerHTML = `
             <td>${serie.id}</td>
             <td>${serie.name}</td>
             <td>${serie.channel}</td>
             <td>${serie.seasons}</td>`;
-        seriesTable.appendChild(trElement);
+            trElement.addEventListener("click", () => {
+                mostrarDetallesSerie(serie);
+            });
+
+            seriesTable.appendChild(trElement);
     });
+    
 }
 
 function getPromedioTemporadas(seriesList: any[]): number {
@@ -25,17 +32,14 @@ function getPromedioTemporadas(seriesList: any[]): number {
 }
 
 function mostrarDetallesSerie(serie: any): void {
-    const detallesSerie = document.getElementById("detalles-serie")!;
-    const imagen = document.createElement("imagen") as HTMLImageElement;
-    const nombre = document.createElement("nombre");
-    const descripcion = document.createElement("descripcion");
-    const link = document.createElement("enlace") as HTMLAnchorElement;
-    
-    imagen.src = serie.imagen;
-    nombre.innerHTML = serie.nombre;
-    descripcion.innerHTML = serie.descripcion;
-    link.href = serie.pagina;
-    link.textContent = serie.pagina;
-    detallesSerie.style.display = "block";
+    detallesSerie.innerHTML = `
+        <div class="card";">
+            <img class="card-img-top" src="${serie.image}" alt="Imagen de ${serie.name}">
+            <div class="card-body">
+                <h5 class="card-title">${serie.name}</h5>
+                <p class="card-text">${serie.description}</p>
+                <a href="${serie.link}" target="_blank" class="btn btn-primary">Ver más</a>
+            </div>
+        </div>    
+    `;
 }
-
